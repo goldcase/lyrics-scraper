befbc912f9713a7e2fa25d8d6bf928c0e457cd6f
Johnny Chang <johnny@johnnychang.com>
2015-08-20 18:28:05 -0700

    added function to create URLs to scrape lyrics from

--- scraper.py
+++ scraper.py
@@ -1,3 +1,30 @@
 #!/usr/bin/python
 
+import sys
+import scrapy
 
+BASE_URL = "http://www.azlyrics.com"
+NUMBER_SEGMENT = "/19"
+
+def build_url(artist):
+    ret = BASE_URL
+
+    if artist[0].isdigit():
+        ret += NUMBER_SEGMENT
+    else:
+        ret += "/" + artist[0]
+    ret += "/" + artist + ".html"
+
+    return ret
+
+def get_urls():
+    urls = []
+    file = open(sys.argv[1])
+
+    # Read in each normalized artist, remove newlines, and get the URL.
+    for line in file:
+        urls.append(build_url(line.replace("\n", "")))
+
+    return urls
+
+print get_urls()
