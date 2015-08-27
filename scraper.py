3b025e5168c17b4e9ee03f9a4f0a3d13f53351ed
Johnny Chang <johnny@johnnychang.com>
2015-08-27 17:54:52 -0500

    saving progress

--- scraper.py
+++ scraper.py
@@ -2,12 +2,18 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 import errno
 
 def set_up_driver(width = 1200, height = 800):
     driver = webdriver.PhantomJS()
     driver.set_window_size(width, height)
+    return driver
+
+def tear_down_driver(driver):
+    driver.quit()
 
 # Contract: Expects a file in the same directory called urls.txt
 def get_urls():
@@ -28,35 +34,42 @@ def create_folder(path):
         if exception.errno != errno.EEXIST:
             raise
 
-def get_artist_from_url(url):
+def get_name_from_url(url):
     return url.split("/")[-1][0:-5]
 
-def scrape_song_lyrics(song_url):
-    pass
+def scrape_song_lyrics(driver, song_url):
+    print song_url
+    driver.get(song_url)
+    # Wait until all elements have loaded (max 10 seconds).
+    song_text = ""
+    try:
+        song_text = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(By.XPATH, "/html/body/div[3]/div/div[2]/div[6]")
+        )
+    finally:
+        return song_text
 
-def scrape_song_urls(driver):
+def scrape_lyrics(driver):
     artists = get_urls()
     songs = {}
 
     for artist in artists:
-        artist_name = get_artist_from_url(artist)
+        artist_name = get_name_from_url(artist)
+        create_folder(artist_name)
         temp_songs = []
         driver.get(artist)
         items = driver.find_elements_by_xpath("//div[@id='listAlbum']/a")
+        driver.implicitly_wait(10)
         for item in items:
-            temp_songs.append(item.get_attribute("href"))
+            url = item.get_attribute("href")
+            if url != None:
+                scrape_song_lyrics(driver, url)
 
         # Add to return dict keyed by artist if content was found
         if (len(temp_songs) > 0):
             songs[artist_name] = temp_songs
 
+    tear_down_driver(driver)
     return songs
 
-def scrape_lyrics(driver):
-    # Songs is a dict of artist to array of URLs of their songs.
-    songs = scrape_song_urls(driver)
-    for artist in songs:
-        print artist
-
-create_folder("drake")
 scrape_lyrics(set_up_driver())
