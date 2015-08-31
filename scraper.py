ccc7678cfef307cbd1b7b39505fc0e1ff5914eed
Johnny Chang <johnny@johnnychang.com>
2015-08-30 23:21:21 -0500

    began big refactoring with beautiful soup; added comments; removed all selenium

--- scraper.py
+++ scraper.py
@@ -1,75 +1,97 @@
 #!/usr/bin/python
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import os
 import errno
+from bs4 import BeautifulSoup
+import urllib2
 
-def set_up_driver(width = 1200, height = 800):
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(width, height)
-    return driver
+URL_FILE = "urls.txt"
 
-def tear_down_driver(driver):
-    driver.quit()
+# Params : urls_path, the path of the file containing urls you want to read.
+# Returns: Array of parsed urls.
 
-# Contract: Expects a file in the same directory called urls.txt
-def get_urls():
+def get_urls(urls_path):
     urls = []
-    file = open("urls.txt")
-
+    file = open(urls_path)
+    # Read each line and append it to the array urls, minus the newline char
     for line in file:
         urls.append(line.replace("\n", ""))
 
     return urls
 
-# Creates directory if it doesn't exist already.
-# Careful of race conditions.
+# Description: Creates directory if it doesn't exist already.
+#            : Includes checking for race conditions (raises exception).
+# Params     : The path you want the folder to be at.
+# Returns    : The path.
+
 def create_folder(path):
+    print "Creating folder: " + path
+
     try:
         os.makedirs(path)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
+    return path
+
+# Description: Creates song file in the directory of the artist with lyrics.
+# Params     : song_text (the lyrics)
+#            : artist_path (the path of the artist's folder)
+#            : song_name (the extracted name of the song)
+# Returns    : the path to the song
+
+def create_file(artist_path, song_name, song_text):
+    print "Creating file: " + artist_path + " " + song_name
+
+    # Function decides for us whether to use "/" or "\".
+    path = os.path.join(artist_path, song_name + ".txt")
+    # Good practice to use "with" when dealing with file objects. Terse and
+    # closes file properly.
+    with open(path, "w") as song_file:
+        # TODO: Figure out if BeautifulSoup returns a string or a list of
+        # strings
+        song_file.write(song_text)
+
+    return path
+
+# Description: Extracts name of artist/song from URL.
+# Params     : url to extract data from
+# Returns    : File name (not including extension).
 
 def get_name_from_url(url):
     return url.split("/")[-1][0:-5]
 
-def scrape_song_lyrics(driver, song_url):
-    print song_url
-    driver.get(song_url)
-    # Wait until all elements have loaded (max 10 seconds).
-    song_text = ""
-    try:
-        song_text = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located(By.XPATH, "/html/body/div[3]/div/div[2]/div[6]")
-        )
-    finally:
-        return song_text
+# Description: Gets song text from song url.
+# Params     : song url and relevant artist path.
+# Returns    : song lyrics.
 
-def scrape_lyrics(driver):
-    artists = get_urls()
-    songs = {}
+def scrape_song_lyrics(song_url, artist_path):
+    print "Scraping song lyrics: " + song_url
 
+    song_name = get_name_from_url(song_url)
+    soup = BeautifulSoup(urllib2.urlopen(song_url).read())
+    song_text = soup.get_text()
+    print song_text
+
+    return song_text
+
+# Description: Scrapes lyrics from a list of urls of artists.
+# Params: List of artist URLs
+# Returns: Nothing
+
+def scrape_lyrics(artist_urls):
     for artist in artists:
         artist_name = get_name_from_url(artist)
+        print "Artist: " + artist_name
         create_folder(artist_name)
         temp_songs = []
-        driver.get(artist)
-        items = driver.find_elements_by_xpath("//div[@id='listAlbum']/a")
-        driver.implicitly_wait(10)
+       # items = driver.find_elements_by_xpath("//div[@id='listAlbum']/a")
         for item in items:
+            print "Found item"
             url = item.get_attribute("href")
             if url != None:
-                scrape_song_lyrics(driver, url)
+                scrape_song_lyrics(url, artist_name)
 
         # Add to return dict keyed by artist if content was found
         if (len(temp_songs) > 0):
             songs[artist_name] = temp_songs
-
-    tear_down_driver(driver)
-    return songs
-
-scrape_lyrics(set_up_driver())
